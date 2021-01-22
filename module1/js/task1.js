b0822b47684f444470cf567a69c2833fdd166fbe
Flakezzz <jura.vj8@gmail.com>
2021-01-22 14:00:29 +0200

    added task1

---
+++ module1/js/task1.js
@@ -0,0 +1,51 @@
+const userName = 'Mango';
+
+console.log(userName);
+
+const age = 14;
+
+console.log(age);
+
+const money = 12000;
+
+console.log(money);
+
+const color = 'red';
+
+console.log(color);
+
+const subject = 'Math';
+
+console.log(subject);
+
+
+
+let job;
+
+job = 'Enginer';
+
+console.log(job);
+
+let car;
+
+car = 'Toyota Supra';
+
+console.log(car);
+
+let hobby;
+
+hobby = 'games';
+
+console.log(hobby);
+
+let weight;
+
+weight = 40;
+
+console.log(weight);
+
+let high;
+
+high = 168;
+
+console.log(high);
