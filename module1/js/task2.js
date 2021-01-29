615d07070c487f0ef731e3f48c503b410744c571
Flakezzz <jura.vj8@gmail.com>
2021-01-29 11:37:46 +0200

    added task2

---
+++ module1/js/task2.js
@@ -0,0 +1,81 @@
+const userName = prompt('Будь-ласка введіть ваше імя')
+
+console.log(userName);
+
+// ---
+
+const myAge = 13;
+
+const friendsAge =15;
+
+alert (myAge);
+
+alert (friendsAge);
+
+// ---
+
+const a = 5;
+
+const b = 8; 
+
+console.log( a + b );
+
+// ---
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+// ---
+
+let a5 = 5 % 3;
+let a6 = 3 % 5;
+let a7 = 5 + '3';
+let a8 ='5' - 3;
+let a9 =75 + 'кг';
+
+alert(a5);
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+
+// ---
+
+const height = 23;
+
+const width = 10;
+
+let s = 0;
+
+s = height * width;
+
+console.log(s);
+
+// ---
+
+const heightC = 10;
+
+const dC = 4;
+
+let v = 0;
+
+v = 10*4;
+
+console.log(v);
+
+// ---
+
+const n = 3;
+
+const m = 4;
+
+const k = Math.sqrt(n**2 + m**2);
+
+console.log(k)
